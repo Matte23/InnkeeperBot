30df20e7c8252631b220aab859dd7872bbd69f2c
Matte23 <matteo.schiff23@gmail.com>
2021-10-29 18:34:47 +0200

    Fix warnings

--- help.go
+++ help.go
@@ -28,27 +28,27 @@ func commandHelp(s *discordgo.Session, m *discordgo.MessageCreate) {
 		Description: "InnkeeperBot allow users to manage custom channels",
 		Color:       0x00ff00,
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!new <channel> [private]",
 				Value: "Create a new channel. Add the \"private\" keyword at the end of the command to make that channel private",
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!del <channel>",
 				Value: "Delete an existing channel",
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!add <channel> <user>",
 				Value: "Give an user the permission to join a channel",
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!rem <channel> <user>",
 				Value: "Remove join permission from an user",
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!op <channel> <user>",
 				Value: "Give an user the permission to join, edit and delete a channel",
 			},
-			&discordgo.MessageEmbedField{
+			{
 				Name:  "!deop <channel> <user>",
 				Value: "Remove join/edit/delete permission from an user",
 			},
